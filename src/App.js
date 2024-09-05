a88b22f089092b4beef75a2ce8285c798c7bd9a3
Pranav Ajith <pranav.ajith.cse21@itbhu.ac.in>
2024-09-05 14:19:55 +0530

    Added components folder, and started implementing TitleBar and Footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import { TitleBar } from './components/general-components/Title';
+import { Footer } from './components/general-components/Footer';
 
 function App() {
   return (
     <>
+    < TitleBar />
+    < Footer />
     </>
   );
 }
