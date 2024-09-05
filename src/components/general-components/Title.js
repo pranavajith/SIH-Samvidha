0b7f8ebe980cf3e6c9c54ea0e010811e7d09c540
Pranav Ajith <pranav.ajith.cse21@itbhu.ac.in>
2024-09-05 14:22:13 +0530

    Added components bar and made relevant changes

---
+++ src/components/general-components/Title.js
@@ -0,0 +1,16 @@
+import './../../styles/Title.css';
+
+const TitleBar = () => {
+    return (
+        <nav className="navbar">
+            <h1 className="navbar-brand">Samvidha</h1>
+            <ul className="navbar-menu">
+                <li className="navbar-item"><a href="#about">About</a></li>
+                <li className="navbar-item"><a href="#contact">Contact</a></li>
+                <li className="navbar-item"><a href="#signin">Sign In</a></li>
+            </ul>
+        </nav>
+    )
+};
+
+export { TitleBar };
