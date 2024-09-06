d2773f5a32218ffc9193549eb5cbc84b1829a4fa
Pranav Ajith <pranav.ajith.cse21@itbhu.ac.in>
2024-09-06 20:24:43 +0530

    SplashScreen implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { TitleBar } from './components/general-components/Title';
+import { Footer } from './components/general-components/Footer';
+import { SplashScreen } from './components/SpashScreen';
 
 function App() {
   return (
-    <>
+    <>  
+    <SplashScreen />
+    <TitleBar />
+    < Footer />
     </>
   );
 }
