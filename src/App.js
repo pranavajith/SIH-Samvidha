dd5a9e87eb150f5ab5ac6dc147daa97076d49b10
Pranav Ajith <pranav.ajith.cse21@itbhu.ac.in>
2024-09-06 20:58:43 +0530

    Added Components for Parts of Constitution

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+// App.js
 import './App.css';
 import { TitleBar } from './components/general-components/Title';
 import { Footer } from './components/general-components/Footer';
 import { SplashScreen } from './components/SpashScreen';
+import { Component1, Component2, Component3, Component4 } from './components/MainComponents';
 
 function App() {
   return (
     <>  
-    <SplashScreen />
-    <TitleBar />
-    < Footer />
+      <SplashScreen />
+      <TitleBar />
+      <Component1 />
+      <div style={{ display: 'flex', width: '100%', height: '50vh' }}>
+        <Component2 />
+        <Component3 />
+        <Component4 />
+      </div>
+      <Footer />
     </>
   );
 }
