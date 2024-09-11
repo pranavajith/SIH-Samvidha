5c0b5a93a205515b69e5b4590c0369cb0ce535da
Pranav Ajith <pranav.ajith.cse21@itbhu.ac.in>
2024-09-11 09:56:27 +0530

    Split HomePage and other components into 'Not Signed In Section'

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import React from 'react';
-import './App.css';
-import { TitleBar } from './components/general-components/Title';
-import { SplashScreen } from './components/SplashScreen';
-import { HomePage } from './components/HomePage/HomePage';
-import { QuestionDisplay } from './components/QuestionDisplay'; // Import your QuestionDisplay component
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ConstitutionTimeline } from './components/ConstitutionTimeline';
-import AboutUs from './components/AboutUs';
-import Signup from './components/SignUp.js';
-import SignIn from './components/SignIn.js';
+import { NotSignedInSection } from './components/NotSignedInSection';
 
 function App() {
+  const UserProfile = null;
   return (
-    <Router>
+    <>
+    {
+      UserProfile ? 
       <div>
-        <SplashScreen />
-        <TitleBar /> 
-        <Routes>
-          <Route path="/" element={<HomePage />} /> 
-          <Route path="/demo" element={<QuestionDisplay />} /> 
-          <Route path="/about" element={<AboutUs />} /> 
-          <Route path="/signup" element={<Signup />} /> 
-          <Route path="/signin" element={<SignIn />} /> 
-        </Routes>
-      </div>
-    </Router>
+        hi
+      </div> 
+      : 
+      <NotSignedInSection />
+    }
+    </>
   );
 }
 
---
+++ src/components/NotSignedInSection.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './../App.css';
+import { TitleBar } from './general-components/Title';
+import { SplashScreen } from './SplashScreen';
+import { HomePage } from './HomePage/HomePage';
+import { QuestionDisplay } from './QuestionDisplay'; // Import your QuestionDisplay component
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ConstitutionTimeline } from './ConstitutionTimeline';
+import AboutUs from './AboutUs';
+import Signup from './SignUp.js';
+import SignIn from './SignIn.js';
+
+const NotSignedInSection = () => {
+  const UserProfile = null;
+  return (
+    <Router>
+      <div>
+        <SplashScreen />
+        <TitleBar /> 
+        <Routes>
+          <Route path="/" element={<HomePage />} /> 
+          <Route path="/demo" element={<QuestionDisplay />} /> 
+          <Route path="/about" element={<AboutUs />} /> 
+          <Route path="/timeline" element={<ConstitutionTimeline />} /> 
+          <Route path="/signup" element={<Signup />} /> 
+          <Route path="/signin" element={<SignIn />} /> 
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export  {NotSignedInSection};
