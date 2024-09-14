094f1aca12584b5cbaf34dcd17f76435ce4257a1
bn-nb <115717071+bn-nb@users.noreply.github.com>
2024-09-14 23:31:16 +0530

    Backend Checkpoint - Auth

--- backend/app/data.py
+++ backend/app/data.py
@@ -3,40 +3,42 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# SQLite3 does not need migrations
-DB_PATH = "/db.sqlite3"
-DB__URL = "sqlite://" + DB_PATH
-
-engine = create_engine(DB__URL, connect_args={"check_same_thread": False})
+engine = create_engine("sqlite:///data/db.sqlite3", connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-
-def db_handler():
+def handle():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
 
-
 class User(Base):
     __tablename__ = "users"
     
     primid = Column(Integer, primary_key=True, index=True)
+    mailid = Column(String, unique=True)
     userid = Column(String, unique=True, index=True)
     fsname = Column(String)
     lsname = Column(String)
     hashed = Column(String)
-    mailid = Column(String)
     quesct = Column(Integer)
     goodqn = Column(Integer)
 
-
 class UserCreate(BaseModel):
-    userid: str
+    fsname: str
+    lsname: str
     passwd: str
     mailid: str
+
+class UserLogin(BaseModel):
+    mailid: str
+    passwd: str
+
+class TokenModel(BaseModel):
+    userid: str
+    access: int # timestamp
 
 # Must be called after models are defined
 Base.metadata.create_all(bind=engine)
