fbc992eb681c39e8df438db24c81284ae1ad9c95
scarl918 <ishika.saini.eee21@itbhu.ac.in>
2024-09-15 21:44:19 +0530

    Added icon in timeline

--- frontend/src/components/SignedInComponents/TypeGame.js
+++ frontend/src/components/SignedInComponents/TypeGame.js
@@ -35,7 +35,7 @@ const TypeGame = ({ displayData, onComplete, handleIncompleteReturn }) => {
         setCurrentKeyWord(keyword);
       } else {
         setTimeout(() => {
-          setDisplayText((prev) => prev + (prev.endsWith(" ") ? " " : " ") + wordsArray[currentWordIndex]);
+          setDisplayText((prev) => prev + (prev.endsWith(" ") ? "" : " ") + wordsArray[currentWordIndex]);
           setCurrentWordIndex((prev) => prev + 1);
         }, 100);
       }
@@ -43,7 +43,7 @@ const TypeGame = ({ displayData, onComplete, handleIncompleteReturn }) => {
       setIsComplete(true);
       setTimeout(() => {
         onComplete();
-      }, 1500);
+      },1500);
     }
   }, [currentWordIndex, isInputMode, wordsArray, keywords, onComplete]);
 
