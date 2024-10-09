a820d1482530ba0a57d6109fac7d58f6478cb701
Pranav Ajith <pranav.ajith.cse21@itbhu.ac.in>
2024-10-09 22:36:52 +0530

    Cleaned up code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { UserProvider } from "./context/UserContext";
 import { BrowserRouter as Router } from "react-router-dom";
 // import { useTranslation } from "react-i18next";
@@ -19,4 +17,3 @@ root.render(
   </Router>
   //</React.StrictMode>
 );
-reportWebVitals();
