bb627e7d23bf979df95dc1d68bea36893c76cc4a
Pranav Ajith <pranav.ajith.cse21@itbhu.ac.in>
2024-10-10 15:58:31 +0530

    Added Join and Create Lobby buttons

--- frontend/src/components/SignedInComponents/MultiGamePlay.js
+++ frontend/src/components/SignedInComponents/MultiGamePlay.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./../../styles/MultiGamePlay.css";
 
 export const MultiGamePlay = () => {
-  return <>hi</>;
+  return (
+    <div className="lobby-button-container">
+      <button className="create-lobby-button">Create Lobby</button>
+      <button className="join-lobby-button">Join Lobby</button>
+    </div>
+  );
 };
 
 export default MultiGamePlay;
